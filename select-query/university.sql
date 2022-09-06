8624627f8e8ea3e62c28fa6fbb6ef5b5b04c5183
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-09-06 16:22:38 +0200

    fixed-sql-syntax

--- select-query/university.sql
+++ select-query/university.sql
@@ -18,19 +18,22 @@
 
 --4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
-     SELECT * FROM `courses`
+     SELECT * 
+     FROM `courses`
      WHERE `period` = 'I semestre' 
      AND `year` = 1;
 
 --5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
-    SELECT * FROM `exams`
+    SELECT * 
+    FROM `exams`
     WHERE `hour` > '14:00:00'
     AND `date` = '2020/06/20'; --
 
 --6. Selezionare tutti i corsi di laurea magistrale (38)
 
-    SELECT * FROM `degrees` 
+    SELECT * 
+    FROM `degrees` 
     WHERE `level` = 'magistrale';
 
 --7. Da quanti dipartimenti è composta l'università? (12)
