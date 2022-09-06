b56efbcebf722ae3c6647ed2ad052c8f8a84a069
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-09-06 16:01:34 +0200

    added-exercise-three

--- select-query/university.sql
+++ select-query/university.sql
@@ -12,6 +12,9 @@
 
 --3. Selezionare tutti gli studenti che hanno più di 30 anni
 
+     SELECT *
+     FROM `students`
+     WHERE `date_of_birth` LIKE '1992%'; --(ho qualche dubbio sulla risoluzione di questo esercizio)
 
 --4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
